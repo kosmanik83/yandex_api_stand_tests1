becc4ae19284c677c97b6f06981303da338871fd
Your Name <you@example.com>
2023-10-29 16:32:20 +0400

    first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVER = 'https://1d394a01-632c-4b74-8b4d-f5e6288622fa.serverhub.praktikum-services.ru'
+CREATE_ORDERS = '/api/v1/orders'
+GET_ORDERS = '/v1/orders/track'
---
+++ sender_stand_request.py
@@ -0,0 +1,22 @@
+import configuration
+import requests
+import data
+
+#Создаю новый заказ
+def post_new_orders(body):
+    return requests.post(configuration.URL_SERVER + configuration.CREATE_ORDERS,
+                         json=body)
+
+#Сохраняю номер трека заказа
+def get_new_track():
+    order_responce = post_new_orders(data.order_body)
+    track = order_responce.json()["track"]
+    return track
+
+#Выполняю запрос на получения заказа по треку заказа.
+def get_order():
+    track = get_new_track()
+    params = data.order_params
+    params['t'] = track
+    return requests.get(configuration.URL_SERVER + configuration.GET_ORDERS,
+                        params=params)
