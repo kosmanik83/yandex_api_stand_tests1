37b9bcb63eb534dc8615fdc468fa91d349a6fb32
Your Name <you@example.com>
2023-11-07 22:26:11 +0400

    Внесены изменения по имени, времени и номеру заказа

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVER = 'https://aa42d759-530b-45e2-98c1-abd2a49115d3.serverhub.praktikum-services.ru'
-CREATE_ORDERS = '/api/v1/orders'
+#Косыгина Мария 9 когорта — Дипломный проект. Инженер по тестированию плюс
+URL_SERVER = 'https://00feaa33-1a28-4e31-bf7b-4f2ea3d08e3a.serverhub.praktikum-services.ru'
+CREATE_ORDERS = '/api/v1/orders/track/'
 GET_ORDERS = '/v1/orders/track'
