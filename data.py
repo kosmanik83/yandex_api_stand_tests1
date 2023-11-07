77451927cbc770268561247e798783dd98a26870
Your Name <you@example.com>
2023-11-07 22:24:15 +0400

    Файл переименов и в него внесены поправки

---
+++ data.py
@@ -0,0 +1,16 @@
+#Косыгина Мария 9 когорта — Дипломный проект. Инженер по тестированию плюс
+order_body = {
+    "firstName": "Egor",
+    "lastName": "Levin",
+    "address": "Moscow, Kutuzovsky prospect, 2",
+    "metroStation": 48,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 24,
+    "deliveryDate": "2023-11-09",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
+
+order_params = {'t': 495197}
